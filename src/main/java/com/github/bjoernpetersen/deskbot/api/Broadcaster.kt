f3d31665017d2434384ec43c7f4383b77dcd8581
Björn Petersen <pheasn@gmail.com>
2017-07-16 18:49:30 +0200

    Search for appropriate network interface before broadcasting

--- src/main/java/com/github/bjoernpetersen/deskbot/api/Broadcaster.kt
+++ src/main/java/com/github/bjoernpetersen/deskbot/api/Broadcaster.kt
@@ -39,11 +39,27 @@ class Broadcaster @Throws(InitializationException::class) constructor(private va
         socket.broadcast = true
         try {
             socket.joinGroup(this.groupAddress)
+            val networkInterface = findNetworkInterface()
+            if (networkInterface != null) {
+                socket.networkInterface = networkInterface
+            }
         } catch (e: IOException) {
             throw InitializationException(e)
         }
 
         start()
+    }
+
+    private fun findNetworkInterface(): NetworkInterface? {
+        for (ifc in NetworkInterface.getNetworkInterfaces()) {
+            if (!ifc.isLoopback
+                    && !ifc.isVirtual
+                    && ifc.isUp
+                    && ifc.supportsMulticast()) {
+                return ifc
+            }
+        }
+        return null
     }
 
     override fun getLogger(): Logger = cachedLogger
