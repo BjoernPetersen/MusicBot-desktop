d428820e13cab8fb4308b639074b66d5dcac7e15
Björn Petersen <pheasn@gmail.com>
2017-07-16 14:14:28 +0200

    Implement swag Util with extension functions

--- src/main/java/com/github/bjoernpetersen/deskbot/api/swag/api/impl/Util.kt
+++ src/main/java/com/github/bjoernpetersen/deskbot/api/swag/api/impl/Util.kt
@@ -18,26 +18,26 @@ private typealias ModelSongEntry = com.github.bjoernpetersen.deskbot.api.swag.mo
 private typealias ModelPlayerState = com.github.bjoernpetersen.deskbot.api.swag.model.PlayerState
 private typealias ModelPlayerStateEnum = com.github.bjoernpetersen.deskbot.api.swag.model.PlayerState.StateEnum
 
-fun convert(song: Song): ModelSong {
+fun Song.convert(): ModelSong {
     val result = ModelSong()
-    result.id = song.id
-    result.providerId = song.providerName
-    result.title = song.title
-    result.description = song.description
-    result.albumArtUrl = song.albumArtUrl.orElse(null)
+    result.id = this.id
+    result.providerId = this.providerName
+    result.title = this.title
+    result.description = this.description
+    result.albumArtUrl = this.albumArtUrl.orElse(null)
     return result
 }
 
-fun convert(entry: QueueEntry): ModelQueueEntry {
+fun QueueEntry.convert(): ModelQueueEntry {
     val queueEntry = ModelQueueEntry()
-    queueEntry.userName = entry.user.name
-    queueEntry.song = convert(entry.song)
+    queueEntry.userName = this.user.name
+    queueEntry.song = this.song.convert()
     return queueEntry
 }
 
-fun convert(entry: SongEntry): ModelSongEntry {
-    val song = convert(entry.song)
-    val user = entry.user
+fun SongEntry.convert(): ModelSongEntry {
+    val song = this.song.convert()
+    val user = this.user
     val userName = user?.name
     val queueEntry = ModelSongEntry()
     queueEntry.userName = userName
@@ -45,13 +45,13 @@ fun convert(entry: SongEntry): ModelSongEntry {
     return queueEntry
 }
 
-fun convert(queue: List<QueueEntry>): List<ModelQueueEntry> = queue.map { convert(it) }
+fun List<QueueEntry>.convert(): List<ModelQueueEntry> = this.map { it.convert() }
 
-fun convert(playerState: PlayerState): ModelPlayerState {
+fun PlayerState.convert(): ModelPlayerState {
     val result = ModelPlayerState()
-    result.state = ModelPlayerStateEnum.valueOf(playerState.state.name)
-    result.songEntry = playerState.entry
-            .map { convert(it) }
+    result.state = ModelPlayerStateEnum.valueOf(this.state.name)
+    result.songEntry = this.entry
+            .map { it.convert() }
             .orElse(null)
     return result
 }
