6d3fae325a6fe54073bfbefa0a8024ac5b20db0c
Björn Petersen <pheasn@gmail.com>
2017-07-21 02:06:53 +0200

    Implement getSupport in PlaybackFactoryWrapper

--- src/main/java/com/github/bjoernpetersen/deskbot/model/PlaybackFactoryWrapper.java
+++ src/main/java/com/github/bjoernpetersen/deskbot/model/PlaybackFactoryWrapper.java
@@ -3,6 +3,8 @@ package com.github.bjoernpetersen.deskbot.model;
 import com.github.bjoernpetersen.jmusicbot.NamedPlugin;
 import com.github.bjoernpetersen.jmusicbot.config.Config;
 import com.github.bjoernpetersen.jmusicbot.config.Config.Entry;
+import com.github.bjoernpetersen.jmusicbot.platform.Platform;
+import com.github.bjoernpetersen.jmusicbot.platform.Support;
 import com.github.bjoernpetersen.jmusicbot.playback.PlaybackFactory;
 import java.io.IOException;
 import java.util.List;
@@ -15,6 +17,12 @@ public class PlaybackFactoryWrapper implements NamedPlugin {
 
   public PlaybackFactoryWrapper(@Nonnull PlaybackFactory playbackFactory) {
     this.playbackFactory = playbackFactory;
+  }
+
+  @Nonnull
+  @Override
+  public Support getSupport(@Nonnull Platform platform) {
+    return playbackFactory.getSupport(platform);
   }
 
   @Nonnull
