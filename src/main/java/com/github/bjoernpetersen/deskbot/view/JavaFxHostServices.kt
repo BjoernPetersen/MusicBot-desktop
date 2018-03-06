d90a21abaa74a6198e8e46e6f84c6ce886611c18
Björn Petersen <pheasn@gmail.com>
2018-03-06 17:19:55 +0100

    Use hostServices correctly

--- src/main/java/com/github/bjoernpetersen/deskbot/view/JavaFxHostServices.kt
+++ src/main/java/com/github/bjoernpetersen/deskbot/view/JavaFxHostServices.kt
@@ -9,7 +9,7 @@ class JavaFxHostServices : HostServices {
 
   override fun openBrowser(url: URL) {
     val configWindow = find(ConfigWindow::class)
-    configWindow.hostServices.showDocument(url.toExternalForm())
+    configWindow.app.hostServices.showDocument(url.toExternalForm())
   }
 
   @Throws(IllegalStateException::class)
