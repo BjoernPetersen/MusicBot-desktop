3abfa86d39df1597112ae5f8771a8d6bce083ce7
Björn Petersen <pheasn@gmail.com>
2019-02-15 22:11:15 +0100

    Don't notify suggesters of played songs they can't handle

--- src/main/kotlin/net/bjoernpetersen/deskbot/impl/SongPlayedNotifierImpl.kt
+++ src/main/kotlin/net/bjoernpetersen/deskbot/impl/SongPlayedNotifierImpl.kt
@@ -1,22 +1,44 @@
 package net.bjoernpetersen.deskbot.impl
 
+import com.google.common.collect.Multimap
+import com.google.common.collect.MultimapBuilder
 import com.google.inject.AbstractModule
 import com.google.inject.Scopes
 import net.bjoernpetersen.musicbot.api.auth.Permission
 import net.bjoernpetersen.musicbot.api.player.SongEntry
 import net.bjoernpetersen.musicbot.api.plugin.management.PluginFinder
+import net.bjoernpetersen.musicbot.api.plugin.management.findDependencies
 import net.bjoernpetersen.musicbot.spi.player.SongPlayedNotifier
+import net.bjoernpetersen.musicbot.spi.plugin.Provider
+import net.bjoernpetersen.musicbot.spi.plugin.Suggester
 import javax.inject.Inject
+import kotlin.reflect.full.isSubclassOf
 
-class SongPlayedNotifierImpl @Inject constructor(
-    private val finder: PluginFinder) : SongPlayedNotifier {
+class SongPlayedNotifierImpl @Inject private constructor(
+    finder: PluginFinder) : SongPlayedNotifier {
+
+    private val suggestersByProviderId: Multimap<String, Suggester> = MultimapBuilder
+        .hashKeys()
+        .arrayListValues()
+        .build()
+
+    init {
+        finder.suggesters.forEach { suggester ->
+            suggester
+                .findDependencies()
+                .filter { it.isSubclassOf(Provider::class) }
+                .map { it.qualifiedName!! }
+                .forEach { suggestersByProviderId.put(it, suggester) }
+        }
+    }
 
     override fun notifyPlayed(songEntry: SongEntry) {
         val permissions = songEntry.user?.permissions ?: return
+
         if (Permission.ALTER_SUGGESTIONS !in permissions) {
             return
         }
-        finder.suggesters.forEach {
+        suggestersByProviderId[songEntry.song.provider.id].forEach {
             it.notifyPlayed(songEntry)
         }
     }
