412a900bf3b6161755d634aca32939f36c07f733
Björn Petersen <pheasn@gmail.com>
2019-02-15 06:03:02 +0100

    Make UserInfo properties public
    Private properties are not serialized.

--- src/main/kotlin/net/bjoernpetersen/deskbot/rest/model/UserInfo.kt
+++ src/main/kotlin/net/bjoernpetersen/deskbot/rest/model/UserInfo.kt
@@ -3,8 +3,7 @@ package net.bjoernpetersen.deskbot.rest.model
 import net.bjoernpetersen.musicbot.api.auth.User
 
 data class UserInfo(
-    private val name: String,
-    private val permissions: List<String>) {
-}
+    val name: String,
+    val permissions: List<String>)
 
 fun User.toInfo(): UserInfo = UserInfo(name, permissions.map { it.label })
