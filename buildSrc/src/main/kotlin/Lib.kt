3bb29267e388ef111a230e55ec5a37d38145f7e7
Björn Petersen <pheasn@gmail.com>
2019-03-01 10:55:49 +0100

    Update dependency versions

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,7 +1,7 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
-    const val KOTLIN_LOGGING = "1.6.24"
-    const val SLF4J = "1.8.0-beta2"
+    const val KOTLIN_LOGGING = "1.6.25"
+    const val SLF4J = "1.8.0-beta4"
 
     const val MUSICBOT = "0.18.1"
 
@@ -14,5 +14,5 @@ object Lib {
     const val JUNIT = "5.4.0"
     const val JUNIT_GUICE = "1.1.0"
     const val MOCK_K = "1.9.1"
-    const val ASSERT_J = "3.11.1"
+    const val ASSERT_J = "3.12.1"
 }
