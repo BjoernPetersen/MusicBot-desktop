496718c549f97da14a0d14e65a29ac87e2fbb9d6
Björn Petersen <pheasn@gmail.com>
2019-03-01 10:49:47 +0100

    Remove openjfx version

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -2,9 +2,7 @@ object Plugin {
     const val VERSIONS = "0.20.0"
 
     const val KOTLIN = "1.3.21"
-    const val OPENJFX = "0.0.7"
     const val DOKKA = "0.9.17"
     const val SPOTBUGS_PLUGIN = "1.6.9"
     const val SPOTBUGS_TOOL = "3.1.11"
-    // const val SHADOW_JAR = "4.0.2"
 }
