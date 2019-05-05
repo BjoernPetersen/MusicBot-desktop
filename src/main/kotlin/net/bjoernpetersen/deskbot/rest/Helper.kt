2afaa0a37acc78bcaa9509a612006d3925fbbf76
Björn Petersen <pheasn@gmail.com>
2019-05-05 11:42:36 +0200

    Check plugin type in Helper

--- src/main/kotlin/net/bjoernpetersen/deskbot/rest/Helper.kt
+++ src/main/kotlin/net/bjoernpetersen/deskbot/rest/Helper.kt
@@ -2,16 +2,17 @@
 
 package net.bjoernpetersen.deskbot.rest
 
+import net.bjoernpetersen.musicbot.spi.plugin.Plugin
 import net.bjoernpetersen.musicbot.spi.plugin.PluginLookup
 import net.bjoernpetersen.musicbot.spi.plugin.Provider
 import net.bjoernpetersen.musicbot.spi.plugin.Suggester
 
 @Throws(NotFoundException::class)
 fun PluginLookup.findProvider(id: String): Provider {
-    return lookup(id) ?: throw NotFoundException()
+    return lookup<Plugin>(id) as? Provider ?: throw NotFoundException()
 }
 
 @Throws(NotFoundException::class)
 fun PluginLookup.findSuggester(id: String): Suggester {
-    return lookup(id) ?: throw NotFoundException()
+    return lookup<Plugin>(id) as? Suggester ?: throw NotFoundException()
 }
