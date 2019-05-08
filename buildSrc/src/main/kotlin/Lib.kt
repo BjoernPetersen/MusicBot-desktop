32150200580e33ccdc3a09faa97d77a0a7e4116b
Björn Petersen <pheasn@gmail.com>
2019-05-08 13:32:15 +0200

    Update Kotlin and -coroutines versions

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,6 +1,6 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
-    const val KOTLIN_COROUTINES = "1.2.0"
+    const val KOTLIN_COROUTINES = "1.2.1"
     const val SLF4J = "1.8.0-beta4"
 
     const val MUSICBOT = "0.22.0-SNAPSHOT"
