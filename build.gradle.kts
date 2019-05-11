5de3fe6c46bc6b9d271fb9d5d09adc813d93d166
Björn Petersen <pheasn@gmail.com>
2019-05-11 21:07:36 +0200

    Add spotless with ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("com.diffplug.gradle.spotless") version Plugin.SPOTLESS
+
     id("com.github.ben-manes.versions") version Plugin.VERSIONS
 
     kotlin("jvm") version Plugin.KOTLIN
@@ -30,6 +32,17 @@ application {
 idea {
     module {
         isDownloadJavadoc = true
+    }
+}
+
+spotless {
+    kotlin {
+        ktlint()
+        endWithNewline()
+    }
+    kotlinGradle {
+        ktlint()
+        endWithNewline()
     }
 }
 
