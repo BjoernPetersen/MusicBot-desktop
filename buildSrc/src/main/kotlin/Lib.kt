3e1387bd50e6d98f9d01276644755f859eab86da
Björn Petersen <pheasn@gmail.com>
2019-08-24 19:41:49 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,6 +1,6 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
-    const val KOTLIN_COROUTINES = "1.2.2"
+    const val KOTLIN_COROUTINES = "1.3.0"
     const val SLF4J = "1.8.0-beta4"
 
     const val MUSICBOT = "0.23.0-SNAPSHOT"
--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,8 +1,8 @@
 object Plugin {
-    const val SPOTLESS = "3.24.1"
+    const val SPOTLESS = "3.24.2"
 
     const val VERSIONS = "0.22.0"
 
-    const val KOTLIN = "1.3.41"
+    const val KOTLIN = "1.3.50"
     const val DOKKA = "0.9.18"
 }
