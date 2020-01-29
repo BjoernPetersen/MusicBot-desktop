f73d16d8ffa9b3f7efc8bf40d807c843a40ec1bb
Björn Petersen <pheasn@gmail.com>
2020-01-29 18:29:28 +0100

    Update dependencies

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,19 +1,19 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
-    const val KOTLIN_COROUTINES = "1.3.1"
+    const val KOTLIN_COROUTINES = "1.3.3"
     const val SLF4J = "1.8.0-beta4"
 
     const val MUSICBOT = "0.24.0-SNAPSHOT"
-    const val SQLITE = "3.28.0"
+    const val SQLITE = "3.30.1"
 
     const val CONTROLS_FX = "8.40.15"
 
-    const val KTOR = "1.2.4"
+    const val KTOR = "1.2.5"
 
-    const val JACKSON = "2.9.9"
+    const val JACKSON = "2.10.2"
 
-    const val JUNIT = "5.5.2"
-    const val JUNIT_GUICE = "1.1.1"
+    const val JUNIT = "5.6.0"
+    const val JUNIT_GUICE = "1.2.1"
     const val MOCK_K = "1.9.3"
-    const val ASSERT_J = "3.13.2"
+    const val ASSERT_J = "3.15.0"
 }
--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,8 +1,8 @@
 object Plugin {
-    const val SPOTLESS = "3.24.2"
-    const val DETEKT = "1.0.1"
+    const val SPOTLESS = "3.27.1"
+    const val DETEKT = "1.5.0"
 
-    const val VERSIONS = "0.25.0"
+    const val VERSIONS = "0.27.0"
 
-    const val KOTLIN = "1.3.50"
+    const val KOTLIN = "1.3.61"
 }
