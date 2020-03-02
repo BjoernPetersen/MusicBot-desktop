754a809429fb368d86a233a4ef0069605a07184a
Björn Petersen <pheasn@gmail.com>
2020-03-02 19:45:56 +0100

    Update Gradle and Plugins

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,8 +1,8 @@
 object Plugin {
     const val SPOTLESS = "3.27.1"
-    const val DETEKT = "1.5.0"
+    const val DETEKT = "1.6.0"
 
-    const val VERSIONS = "0.27.0"
+    const val VERSIONS = "0.28.0"
 
     const val KOTLIN = "1.3.61"
 }
