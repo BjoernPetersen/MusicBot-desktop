1fe00d425f971609c8d1a658b56f79231113ae96
Björn Petersen <pheasn@gmail.com>
2020-03-08 13:52:18 +0100

    Use new CircleCI API

--- .circleci/load_artifact.py
+++ .circleci/load_artifact.py
@@ -3,7 +3,6 @@ import json
 import os
 
 import requests
-from circleclient import circleclient
 
 
 def check_not_none(o, m):
@@ -28,33 +27,69 @@ def pretty_print(j):
     print(pretty(j))
 
 
+class Circle:
+    _baseUrl = "https://circleci.com/api/v2"
+
+    def __init__(self, token):
+        self._token = token
+        self._headers = {
+            'Circle-Token': token
+        }
+
+    def get_pipelines(self, slug, branch):
+        url = f"{self._baseUrl}/project/{slug}/pipeline?branch={branch}"
+        response = requests.get(url, headers=self._headers)
+        return response.json()['items']
+
+    def get_workflows(self, pipeline_id):
+        url = f"{self._baseUrl}/pipeline/{pipeline_id}/workflow"
+        response = requests.get(url, headers=self._headers)
+        return response.json()['items']
+
+    def get_jobs(self, workflow_id):
+        url = f"{self._baseUrl}/workflow/{workflow_id}/job"
+        response = requests.get(url, headers=self._headers)
+        return response.json()['items']
+
+    def get_artifacts(self, slug, build_num):
+        url = f"{self._baseUrl}/project/{slug}/{build_num}/artifacts"
+        response = requests.get(url, headers=self._headers)
+        return response.json()['items']
+
+
 class Params:
     def __init__(self, args):
         self.user = check_not_none(args.user, "User must not be empty")
         self.project = check_not_none(
             args.project, "Project needs to be specified")
         self.branch = check_not_none(args.branch, "Branch must not be empty")
-        self.path = check_not_none(
-            args.path, "Artifact path must be specified")
         self.token = check_not_none(
             get_token(args.token), "Token must be specified")
 
     def __repr__(self) -> str:
         return "[user=" + self.user \
                + ", project=" + self.project \
-               + ", branch=" + self.branch \
-               + ", path=" + self.path + "]"
+               + ", branch=" + self.branch + "]"
 
     def __str__(self) -> str:
         return self.__repr__()
 
 
-def find_most_recent_successful(client, user, project, branch):
-    recents = client.build.recent(user, project, branch=branch)
-    for build in recents:
-        if build['status'] == "success":
-            return build['build_num']
-    return None
+def create_slug(user, project) -> str:
+    return f"github/{user}/{project}"
+
+
+def find_most_recent_workflow(client, slug, branch) -> str:
+    for pipeline in client.get_pipelines(slug, branch):
+        workflow = client.get_workflows(pipeline['id'])[0]
+        if workflow['status'] == "success":
+            return workflow['id']
+    print("Could not find successful workflow")
+    exit(1)
+
+
+def find_jobs(client, workflow_id):
+    return client.get_jobs(workflow_id)
 
 
 def download(url, output):
@@ -63,29 +98,26 @@ def download(url, output):
         f.write(response.content)
 
 
-def load_artifact(client: circleclient.CircleClient, user, project, build_num, path):
-    arts = client.build.artifacts(user, project, build_num)
+def load_artifacts(client: Circle, slug, build_num):
+    arts = client.get_artifacts(slug, build_num)
     for artifact in arts:
-        if artifact['path'] == path:
-            download(artifact['url'], os.path.basename(path))
-            return
-    print("No artifact with path " + path + " found")
-    exit(1)
+        path = artifact['path']
+        download(artifact['url'], os.path.basename(path))
 
 
 def run(params: Params):
     user = params.user
     project = params.project
     branch = params.branch
-    path = params.path
 
-    client = circleclient.CircleClient(params.token)
-    most_recent = find_most_recent_successful(client, user, project, branch)
-    if most_recent:
-        load_artifact(client, user, project, most_recent, path)
-    else:
-        print("No successful build found.")
-        exit(1)
+    client = Circle(params.token)
+    slug = create_slug(user, project)
+    workflow_id = find_most_recent_workflow(client, slug, branch)
+    jobs = find_jobs(client, workflow_id)
+
+    for job in jobs:
+        if job['name'].startswith("build_"):
+            load_artifacts(client, slug, job['job_number'])
 
 
 def main():
@@ -106,16 +138,11 @@ def main():
                         action="store",
                         default="master",
                         help="A branch to load from")
-    parser.add_argument("--artifact", "-a",
-                        dest="path",
-                        action="store",
-                        required=True,
-                        help="The artifact path")
     parser.add_argument("--token", "-t",
                         dest="token",
                         action="store",
-                        help="An API token with artifact read permission. Can be left empty and set as CIRCLE_TOKEN "
-                        "environment variable instead")
+                        help="An API token (currently personal access token). Can be left empty and"
+                             " set as CIRCLE_TOKEN environment variable instead")
     args = parser.parse_args()
 
     try:
