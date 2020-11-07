53343d54c0b617a5bc9bea369f7a90b25944d581
Björn Petersen <pheasn@gmail.com>
2020-11-07 14:07:40 +0100

    Update versions of Kotlin and Dokka

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,9 +1,9 @@
 object Plugin {
-    const val SPOTLESS = "3.27.2"
+    const val SPOTLESS = "5.7.0"
     const val KTLINT = "0.39.0"
-    const val DETEKT = "1.6.0"
+    const val DETEKT = "1.14.2"
 
-    const val VERSIONS = "0.28.0"
+    const val VERSIONS = "0.34.0"
 
-    const val KOTLIN = "1.3.70"
+    const val KOTLIN = "1.4.10"
 }
