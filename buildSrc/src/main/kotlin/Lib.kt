eb831057b56eb5dddb5c033e219a485c84eee1cc
Björn Petersen <pheasn@gmail.com>
2020-11-07 14:16:44 +0100

    Update MusicBot to 0.26.0-SNAPSHOT

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -3,7 +3,7 @@ object Lib {
     const val KOTLIN_COROUTINES = "1.3.4"
     const val SLF4J = "1.8.0-beta4"
 
-    const val MUSICBOT = "0.25.0"
+    const val MUSICBOT = "0.26.0-SNAPSHOT"
     const val SQLITE = "3.30.1"
 
     const val CONTROLS_FX = "8.40.15"
