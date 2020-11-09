c66cdad693ae982d51780d3a9dbee5c1b1909820
Björn Petersen <bjoern.petersen@cosee.biz>
2020-11-09 15:51:15 +0100

    Fix style issue

--- src/main/kotlin/net/bjoernpetersen/deskbot/impl/FileConfigStorage.kt
+++ src/main/kotlin/net/bjoernpetersen/deskbot/impl/FileConfigStorage.kt
@@ -63,7 +63,7 @@ class FileConfigStorage(private val configDir: File) : ConfigStorageAdapter {
                     throw IOException("Could not create file")
                 }
             } catch (e: IOException) {
-                logger.error(e) { "Could not create config file '${file}'" }
+                logger.error(e) { "Could not create config file '$file'" }
                 return
             }
         }
